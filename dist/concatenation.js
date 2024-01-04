6a3dc1934f86fc87c17ea304307f7ad9e3539f7e
Sergey Kuran <kuran.serg@gmail.com>
2024-01-04 23:23:30 +0200

    add configure files

---
+++ dist/concatenation.js
@@ -0,0 +1,5 @@
+function concatenation(first, second) {
+    console.log('obje :>> ', `${first} ${second}`);
+}
+export { concatenation };
+//# sourceMappingURL=concatenation.js.map
---
+++ src/concatenation.ts
@@ -0,0 +1,5 @@
+function concatenation(first: string, second: string) {
+    console.log('obje :>> ', `${first} ${second}`);
+}
+
+export {concatenation}
